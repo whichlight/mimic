6b8fd91c8240637e275dcc7a96ea3d319bb38bb8
whichlight <whichlight@gmail.com>
2013-03-19 01:17:24 -0400

    added logging option

--- mimic.py
+++ mimic.py
@@ -1,15 +1,32 @@
+#!/usr/bin/python
+
 from imgproc import *
 import LPD8806
+import datetime
+from optparse import OptionParser
+
+parser= OptionParser()
+parser.add_option("-d", "--data", 
+		help="write data to stdout",
+		default=False)
+
+(opts, args) = parser.parse_args()
+
 
 print "Starting MIMIC..."
 my_cam = Camera(320,240)
 led = LPD8806.strand()
 
-while 0 < 1:
+while True:
 	my_img = my_cam.grabImage()
 	red, green, blue = my_img[160, 120]
-	my_img[160, 120] = 0, 0, 0	
 	led.fill(red, green, blue)
+	led.update()
+	if (opts.data):
+		print "\t".join(map(str,
+			[datetime.datetime.now(),
+			red,green,blue]))
+	
 	
 	
 	
